225d0400d7b9132deeb1fab2353308362bf9e880
andresatierf <andrecfreitas58gmail.com>
2021-12-09 20:42:29 -0100

    Start match

--- backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/model/Match.java
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/model/Match.java
@@ -1,7 +1,17 @@
 package org.academiadecodigo.bootcamp65.model;
 
+import java.sql.Time;
+import java.util.Date;
+
 public class Match {
 
-    private Integer user1;
-    private Integer user2;
+    private Integer user1Id;
+    private Integer user2Id;
+    private Integer roomId;
+
+    private Date date;
+    private double finalPrice;
+
+    private Time time;
+    private Time duration;
 }
