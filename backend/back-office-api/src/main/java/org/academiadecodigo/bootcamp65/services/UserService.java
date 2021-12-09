d988c329518ed270b9aff7194a9f37c3c8272220
jp.pereira <jpvp.pedro9@gmail.com>
2021-12-09 18:40:05 -0100

    add UserService class

---
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/services/UserService.java
@@ -0,0 +1,13 @@
+package org.academiadecodigo.bootcamp65.services;
+
+import org.academiadecodigo.bootcamp65.model.User;
+
+import java.util.List;
+
+public class UserService {
+
+    private List<User> userList;
+
+
+
+}
