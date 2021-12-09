44cb67059b05185093fa4c3666ed3e747c880be7
Code Cadet <codecadet@hamilton.local>
2021-12-09 22:21:56 -0100

    interface imlpementation

--- backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/services/CarServiceImpl.java
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/services/CarServiceImpl.java
@@ -4,7 +4,7 @@ import org.academiadecodigo.bootcamp65.model.Car;
 
 import java.util.*;
 
-public class CarServiceImpl {
+public class CarServiceImpl implements CarService {
 
     private Map<Integer, Car> carMap = new HashMap<>();
 
@@ -12,6 +12,7 @@ public class CarServiceImpl {
         return carMap.isEmpty() ? 1 : Collections.max(carMap.keySet()) + 1;
     }
 
+    @Override
     public Car save(Car car) {
 
         if (car.getId() == null) {
@@ -22,14 +23,17 @@ public class CarServiceImpl {
         return car;
     }
 
+    @Override
     public Car get(Integer id) {
         return carMap.get(id);
     }
 
+    @Override
     public void delete(Integer id) {
         carMap.remove(id);
     }
 
+    @Override
     public List<Car> list() {
         return new ArrayList<>(carMap.values());
     }
