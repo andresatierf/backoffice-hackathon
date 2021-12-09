ce5866150e2a4412716828300d33ea454cd86818
codecadet <lcamara0916@gmail.com>
2021-12-09 19:21:15 -0100

    Creation of branch rooms, implementation of methos to create and delete rooms

---
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/model/RoomType.java
@@ -0,0 +1,18 @@
+package org.academiadecodigo.bootcamp65.model;
+
+public enum RoomType {
+    CEO_OFFICE(100),
+    REGULAR_OFFICE(50),
+    CAFETERIA(25),
+    BAR(25),
+    BATHROOM(15),
+    GARAGE(10),
+    CLEANING_ROOM(10),
+    HALL(10),
+    ELEVATOR(10),
+    COMMON_ROOM(35),
+    ARCHIVE(10),
+    SERVER_ROOM(10);
+
+    RoomType(double price){}
+}
---
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/services/RoomsManager.java
@@ -0,0 +1,54 @@
+package org.academiadecodigo.bootcamp65.services;
+
+import org.academiadecodigo.bootcamp65.model.Room;
+import org.academiadecodigo.bootcamp65.model.RoomType;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RoomsManager {
+
+    private LinkedList<Room> roomsList;
+    private LinkedList<Place> placeList;
+
+    private PlaceService placeService;
+
+    private Place place;
+
+    public void set(PlaceService placeService){
+        this.placeService = placeService;
+    }
+
+    public void createRoom(int placeId, RoomType roomType){
+
+        placeList = placeService.list();
+
+        place = placeList.get(placeId);
+
+        roomsList = place.getRooms();
+
+        int roomNr = roomsList.size();
+
+        Room room = new Room();
+        room.setId(roomNr++);
+        room.setRoomType(roomType);
+
+        place.addRoom(room);
+
+    }
+
+    public void deleteRoom(int placeId, int roomId){
+
+        placeList = placeService.list();
+
+        place = placeList.get(placeId);
+
+        roomsList = place.getRooms();
+
+        roomsList.remove(roomId);
+
+        place.setList(roomsList);
+
+    }
+}
+
