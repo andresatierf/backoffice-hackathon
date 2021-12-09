ce7bbef26c54c18354450be7c3f38ac4550569b7
jp.pereira <jpvp.pedro9@gmail.com>
2021-12-09 18:39:48 -0100

    add User model

---
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/model/User.java
@@ -0,0 +1,108 @@
+package org.academiadecodigo.bootcamp65.model;
+
+import java.util.List;
+
+public class User {
+    private String img;
+    private String name;
+    private int age;
+    private GenderType gender;
+
+    private int minAge;
+    private int maxAge;
+
+    private int lat;
+    private int lon;
+
+    private int rating;
+    private int time;
+    private int duration;
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public GenderType getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderType gender) {
+        this.gender = gender;
+    }
+
+    public int getMinAge() {
+        return minAge;
+    }
+
+    public void setMinAge(int minAge) {
+        this.minAge = minAge;
+    }
+
+    public int getMaxAge() {
+        return maxAge;
+    }
+
+    public void setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+    }
+
+    public int getLat() {
+        return lat;
+    }
+
+    public void setLat(int lat) {
+        this.lat = lat;
+    }
+
+    public int getLon() {
+        return lon;
+    }
+
+    public void setLon(int lon) {
+        this.lon = lon;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
