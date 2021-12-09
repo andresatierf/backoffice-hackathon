e90d70fcb9ad8654ad3c0e237cd81dcc12aab80e
andresatierf <andrecfreitas58gmail.com>
2021-12-09 18:43:33 -0100

    Add Place model and service

---
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/model/Place.java
@@ -0,0 +1,43 @@
+package org.academiadecodigo.bootcamp65.model;
+
+public class Place {
+
+    private Integer id;
+    private String name;
+
+
+    private double lat;
+    private double lon;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
---
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/services/PlaceService.java
@@ -0,0 +1,27 @@
+package org.academiadecodigo.bootcamp65.services;
+
+import org.academiadecodigo.bootcamp65.model.Place;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PlaceService {
+
+    List<Place> places = new LinkedList<>();
+
+    public List<Place> list() {
+        return places;
+    }
+
+    public Place get(int id) {
+        return places.get(id);
+    }
+
+    public void add(Place place) {
+        places.add(place);
+    }
+
+    public void remove(int id) {
+        places.remove(places.get(id));
+    }
+}
