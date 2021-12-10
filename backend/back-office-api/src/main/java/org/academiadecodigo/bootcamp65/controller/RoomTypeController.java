b08e7715bb2d044dc8ff4b2302ac69a82a667c3d
jp.pereira <jpvp.pedro9@gmail.com>
2021-12-10 05:22:16 -0100

    add RoomType to roomtype getter

---
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/controller/RoomTypeController.java
@@ -0,0 +1,25 @@
+package org.academiadecodigo.bootcamp65.controller;
+
+import org.academiadecodigo.bootcamp65.command.UserDto;
+import org.academiadecodigo.bootcamp65.model.RoomType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/roomtype")
+public class RoomTypeController {
+
+    @RequestMapping(method = RequestMethod.GET, path = {"/", ""})
+    public ResponseEntity<List> listRooms() {
+        return new ResponseEntity<>(RoomType.roomList(), HttpStatus.OK);
+    }
+
+}
