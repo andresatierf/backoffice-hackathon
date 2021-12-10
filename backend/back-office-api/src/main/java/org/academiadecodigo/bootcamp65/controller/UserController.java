5754916e1f50416098fbfc5be0b62467394b0473
jp.pereira <jpvp.pedro9@gmail.com>
2021-12-10 05:30:02 -0100

    fix UserController

--- backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/controller/UserController.java
+++ backend/back-office-api/src/main/java/org/academiadecodigo/bootcamp65/controller/UserController.java
@@ -17,6 +17,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
